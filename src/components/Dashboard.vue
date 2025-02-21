d398843e1b53b5d927457099984a699a8a82250f
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2025-02-21 18:12:21 +0800

    maka logout na

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="dashboard-container text-center">
     <!-- Title -->
-    <h1 class="font-weight-bold my-4">Dashboard</h1>
+    <h1 class="font-weight-bold my-4">{{ name }}</h1>
 
     <!-- Subtitle (Email Address) -->
     <h5 class="text-muted">your-email@example.com</h5>
 
     <!-- DataTable Section (Placeholder) -->
-    <b-container class="mt-5">
-      <b-card class="shadow-sm p-4">
-        <p class="text-muted">DataTable Section</p>
-      </b-card>
-    </b-container>
+
   </div>
 
 
@@ -19,12 +15,12 @@
   <div @click="logout" class="btn btn-primary">logout</div>
 </template>
 
-<script>
-export default {
-  name: "Dashboard",
-};
-localStorage.removeItem("authToken");
 
+<script setup>
+
+import router from '@/router';
+
+const name = 'Dashboard';
 
 const logout = () => {
   console.log("nigana naaa");
